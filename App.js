619b0f4c8c7ae41d9dd21e0ad1768f6b783eadbd
shanmukhi406 <157720106+shanmukhi406@users.noreply.github.com>
2024-09-06 12:12:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,46 @@
+// App..js
+
+import './App.css';
+import { useState } from 'react';
+import ReactPlayer from 'react-player';
+import movieTrailer from 'movie-trailer';
+
+function App() {
+
+    //Setting up the initial states using
+    // react hook 'useState"
+    const [video, setVideo] = useState("inception");
+    const [videoURL, setVideoURL] =
+        useState("https://youtu.be/sa9l-dTv9Gk");
+
+    //A function to fetch the required URL
+    // and storing it inside the
+    // videoURL state variable
+    function handleSearch() {
+        movieTrailer(video).then((res) => {
+            setVideoURL(res);
+        });
+    }
+
+    return (
+        <div className="App">
+            <div className="search-box">
+                <label>
+                    Search for any movies/shows:{" "}
+                </label>
+                <input type="text" onChange=
+                    {(e) => { setVideo(e.target.value) }} />
+
+                <button onClick={() => { handleSearch() }}>
+                    Search
+                </button>
+            </div>
+
+      // Using 'ReactPlayer' component to
+            // display the video
+            <ReactPlayer url={videoURL} controls={true} />
+        </div>
+    );
+}
+
+export default App;
